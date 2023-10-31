6321c581dcb6c5da19ae00619511b573aa0616d4
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2023-10-31 00:58:22 -0600

    Minor updates

--- app/week7/meal-ideas.js
+++ app/week7/meal-ideas.js
@@ -1,4 +1,3 @@
-"use client"
 import React, { useState, useEffect } from 'react';
 import './res/styles.css';
 
@@ -9,31 +8,29 @@ const fetchMealIdeas = async (ingredient) => {
 };
 
 const fetchMealDetails = async (id) => {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-    const data = await response.json();
-    return data.meals[0];
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+  const data = await response.json();
+  return data.meals[0];
+};
+
+const MealIdeas = ({ ingredient }) => {
+  const [meals, setMeals] = useState([]);
+  const [mealDetails, setMealDetails] = useState(null);  // Added this line
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const loadMealIdeas = async () => {
+    const fetchedMeals = await fetchMealIdeas(ingredient);
+    if (fetchedMeals) {
+      setMeals(fetchedMeals);
+    }
   };
-  const loadMealDetails = async (id) => {
+
+  const loadMealDetails = async (id) => {  // Moved this function inside the component
     const details = await fetchMealDetails(id);
     setMealDetails(details);
   };
 
-const MealIdeas = ({ ingredient }) => {
-  const [meals, setMeals] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(true); // Add this line
-
-  const loadMealIdeas = async () => {
-    try {
-      const fetchedMeals = await fetchMealIdeas(ingredient);
-      if (fetchedMeals) { // Check if fetchedMeals is not null or undefined
-        setMeals(fetchedMeals);
-      }
-    } catch (error) {
-      console.error('Error fetching meals:', error);
-    }
-  };
-
-  const closeModal = () => { // Add this function
+  const closeModal = () => {
     setIsModalOpen(false);
   };
 
@@ -44,24 +41,38 @@ const MealIdeas = ({ ingredient }) => {
   return (
     <>
       {isModalOpen && (
-        <div className="modal" onClick={closeModal}>
-          <div className="modal-content" onClick={e => e.stopPropagation()}>
-            <h1>Here are some meal ideas using {ingredient}</h1>
-            <ul>
+        <div className="modal flex items-center justify-center h-screen bg-opacity-50 bg-black fixed inset-0 overflow-y-auto" onClick={closeModal}>
+          <div className="modal-content bg-grey-200 p-6 rounded-lg w-1/2" onClick={e => e.stopPropagation()}>
+            <h1 className="text-2xl mb-4 text-center">Here are some meal ideas using {ingredient}</h1>
+            <ul className={`flex ${meals.length > 5 ? 'overflow-x-auto' : ''} divide-y divide-gray-300`} >
                 {meals && meals.length > 0 ? (
-                meals.map((meal) => (
-                    <li key={meal.idMeal} onClick={() => loadMealDetails(meal.idMeal)}>{meal.strMeal}</li>
-                ))
-            ) : (
-                <li>No meals found for this ingredient.</li>
-              )}
+                    meals.map((meal) => (
+                    <li className="py-2 px-4 hover:bg-blue-100 cursor-pointer text-center" key={meal.idMeal} onClick={() => loadMealDetails(meal.idMeal)}>
+                        {meal.strMeal}
+                    </li>
+                    ))
+                ) : (
+                    <li>No meals found for this ingredient.</li>
+                )}
             </ul>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={closeModal}>Close</button>
+
+            <div className=" mt-6">
+              <h1 className="text-2xl mb-4">Meal Details</h1>
+              {mealDetails && (
+                <div>
+                  <h2 className="text-xl mb-2">{mealDetails.strMeal}</h2>
+                    <div className="flex flex-column ml-4">
+                        <img className="w-1/6 object-cover rounded-lg m-4" src={mealDetails.strMealThumb} alt={mealDetails.strMeal} />
+                        <p className="text-sm">{mealDetails.strInstructions}</p>
+                    </div>
+                </div>
+              )}
+            </div>
+            <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={closeModal}>Close</button>
           </div>
         </div>
       )}
     </>
   );
 };
-
 export default MealIdeas;
