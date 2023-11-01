dd07e0e255ddeb52dda5ca5de696518ccc2fe6d6
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2023-11-01 14:08:55 -0600

    Major update

--- app/week7/meal-ideas.js
+++ app/week7/meal-ideas.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './res/styles.css';
+import 'font-awesome/css/font-awesome.min.css';
 
+  
+//ingredient fetching part
 const fetchMealIdeas = async (ingredient) => {
   const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
   const data = await response.json();
@@ -14,6 +17,7 @@ const fetchMealDetails = async (id) => {
 };
 
 const MealIdeas = ({ ingredient }) => {
+
   const [meals, setMeals] = useState([]);
   const [mealDetails, setMealDetails] = useState(null);  // Added this line
   const [isModalOpen, setIsModalOpen] = useState(true);
@@ -41,7 +45,7 @@ const MealIdeas = ({ ingredient }) => {
   return (
     <>
       {isModalOpen && (
-        <div className="modal flex items-center justify-center h-screen bg-opacity-50 bg-black fixed inset-0 overflow-y-auto" onClick={closeModal}>
+        <div className="modal flex items-center justify-center h-auto bg-opacity-50 bg-black fixed inset-0 overflow-y-auto" onClick={closeModal}>
           <div className="modal-content bg-grey-200 p-6 rounded-lg w-1/2" onClick={e => e.stopPropagation()}>
             <h1 className="text-2xl mb-4 text-center">Here are some meal ideas using {ingredient}</h1>
             <ul className={`flex ${meals.length > 5 ? 'overflow-x-auto' : ''} divide-y divide-gray-300`} >
@@ -61,6 +65,7 @@ const MealIdeas = ({ ingredient }) => {
                 {mealDetails && (
                     <div>
                         <h2 className="text-xl mb-2">{mealDetails.strMeal}</h2>
+                        
                         <div className="flex">
                             <img className="w-1/3 object-cover rounded-lg m-4 border-4 border-blue-400" src={mealDetails.strMealThumb} alt={mealDetails.strMeal} />
                             <div className="text-sm ml-4">
@@ -74,8 +79,9 @@ const MealIdeas = ({ ingredient }) => {
                     </div>
                 )}
             </div>
-
-            <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={closeModal}>Close</button>
+            <div className="flex">
+                <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={closeModal}>Close</button>
+           </div>
           </div>
         </div>
       )}
